b7e948ad24310d63f8b320167d5f8a1556fa975e
Alan Bradley <alan@gangleri.net>
2014-02-07 15:04:32 +0000

    modify implemented

--- test/setup-ldap-server.js
+++ test/setup-ldap-server.js
@@ -73,7 +73,6 @@ function buildLdapServer(db, suffix) {
       }
 
       node = node[key]
-
       index--
     }
 
@@ -103,7 +102,6 @@ function buildLdapServer(db, suffix) {
       }
 
       node = node[key]
-
       index--
     }
 
@@ -111,77 +109,68 @@ function buildLdapServer(db, suffix) {
     return next()
   })
 
-  // server.compare(suffix, authorize, function(req, res, next) {
-  //   var dn = req.dn.toString()
-  //   if (!db[dn])
-  //     return next(new ldap.NoSuchObjectError(dn))
-
-  //   if (!db[dn][req.attribute])
-  //     return next(new ldap.NoSuchAttributeError(req.attribute))
-
-  //   var matches = false
-  //   var vals = db[dn][req.attribute]
-  //   for (var i = 0; i < vals.length; i++) {
-  //     if (vals[i] === req.value) {
-  //       matches = true
-  //       break
-  //     }
-  //   }
-
-  //   res.end(matches)
-  //   return next()
-  // })
-
   server.modify(suffix, authorize, function(req, res, next) {
     var dn = req.dn.toString()
+    var path = dn.split(',')
+    var index = path.length -1
+    var node = db 
+
     if (!req.changes.length)
       return next(new ldap.ProtocolError('changes required'))
-    if (!db[dn])
-      return next(new ldap.NoSuchObjectError(dn))
 
-    var entry = db[dn]
+    while(index >= 0) {
+      var key = path[index].trim()
+      node = node[key]
 
-    for (var i = 0; i < req.changes.length; i++) {
-      mod = req.changes[i].modification
-      switch (req.changes[i].operation) {
-        case 'replace':
-          if (!entry[mod.type])
-            return next(new ldap.NoSuchAttributeError(mod.type))
+      if(!node)
+        return next(new ldap.NoSuchObjectError(dn))
 
-          if (!mod.vals || !mod.vals.length) {
-            delete entry[mod.type]
-          } else {
-            entry[mod.type] = mod.vals
+      if(index === 0) {
+        for(var i = 0; i < req.changes.length; i++) {
+          var change = req.changes[i]
+          var mod = change.modification
+
+          switch(change.operation) {
+            case 'replace' :
+              if (!node[mod.type])
+                return next(new ldap.NoSuchAttributeError(mod.type));
+
+              if (!mod.vals || !mod.vals.length) {
+                delete node[mod.type];
+              } else {
+                node[mod.type] = mod.vals;
+              }
+
+              break
+
+            case 'add':
+              if (!node[mod.type]) {
+                node[mod.type] = mod.vals;
+              } else {
+                mod.vals.forEach(function(v) {
+                  if (node[mod.type].indexOf(v) === -1)
+                    node[mod.type].push(v);
+                });
+              }
+              break
+
+            case 'delete':
+              if (!entry[mod.type])
+                return next(new ldap.NoSuchAttributeError(mod.type));
+
+              delete entry[mod.type];
+
+              break
           }
-
-          break
-
-        case 'add':
-          if (!entry[mod.type]) {
-            entry[mod.type] = mod.vals
-          } else {
-            mod.vals.forEach(function(v) {
-              if (entry[mod.type].indexOf(v) === -1)
-                entry[mod.type].push(v)
-            })
-          }
-
-          break
-
-        case 'delete':
-          if (!entry[mod.type])
-            return next(new ldap.NoSuchAttributeError(mod.type))
-
-          delete entry[mod.type]
-
-          break
+        }
       }
+      index--
     }
 
     res.end()
     return next()
-  })
 
+  })
 
   return server
 }
