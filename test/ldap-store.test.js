1bf165c6e1d941e6582ffd8955a476e6f9db533f
Alan Bradley <alan@gangleri.net>
2014-02-10 21:45:33 +0000

    test infrastructure

--- test/ldap-store.test.js
+++ test/ldap-store.test.js
@@ -2,6 +2,36 @@
 "use strict"
 
 var test = require('tap').test
+var ldap = require('ldapjs')
 var setup = require('./setup-ldap-server.js').setupTestLdapServer
-var port =  require('./setup-ldap-server.js').PORT
+var PORT =  require('./setup-ldap-server.js').PORT
+var seneca = require('seneca')
+
+function initSeneca() {
+  var si = seneca()
+  si.use('..', {
+    url: 'ldap://127.0.0.1:' + PORT,
+    password: 'secret',
+    cn: 'cn=root'
+  })
+  return si
+}
+
+
+test('Write entity to ldap', function(t) {
+  setup({}, function(err, server, db) {
+    var si = initSeneca()
+    var e = si.make$()
+    e.dn = 'cn=foo, ou=users, o=example'
+    e.objectClass= 'unixUser'
+    e.cn = 'ldapjs'
+    e.description = 'Created via ldapadd'
+    e.save$(function(error, entity) {
+      si.close(function(err) {
+      })
+      server.close()
+      t.end()
+    })
+  })
+})
 
--- test/setup-ldap-server.js
+++ test/setup-ldap-server.js
@@ -18,9 +18,6 @@ module.exports.setupTestLdapServer = function(opts, done) {
 
 // authorization function to be used by all server handlers
 function authorize(req, res, next) {
-  if (!req.connection.ldap.bindDN.equals('cn=root'))
-    return next(new ldap.InsufficientAccessRightsError())
-
   return next()
 }
 
@@ -28,8 +25,9 @@ function buildLdapServer(db, suffix) {
   var server = ldap.createServer()
 
   server.bind('cn=root', function(req, res, next) {
-    if(req.dn.toString() !== 'cn=root' || req.credentials !== 'secret')
+    if(req.dn.toString() !== 'cn=root' || req.credentials !== 'secret') {
       return next(new ldap.InvalidCredentialsError())
+    }
 
     res.end()
     return next()
